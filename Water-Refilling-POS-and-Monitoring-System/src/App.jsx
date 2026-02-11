759edd9be1a74596b42b1e7a77071654a335e740
Rishi Zurita <zuritarishi@gmail.com>
2026-02-11 23:56:06 +0800

    Login Page

--- Water-Refilling-POS-and-Monitoring-System/src/App.jsx
+++ Water-Refilling-POS-and-Monitoring-System/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './styles/App.css'
+import React from "react";
+import "./styles/App.css";
+import loginBackground from "./backgrounds/loginpcbackground.jpg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div
+      className="login-container"
+      style={{
+        backgroundImage: `url(${loginBackground})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh",
+      }}
+    >
+      <div className="login-box">
+        <h2>User Login</h2>
+        <form>
+          <div className="input-group">
+            <label>Username</label>
+            <input type="text" placeholder="Enter username" />
+          </div>
+          <div className="input-group">
+            <label>Password</label>
+            <input type="password" placeholder="Enter password" />
+          </div>
+          <button type="submit">Login</button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
